ed36e5d386cd0e4dbe6b69b4b70550b60247e7ff
canaelgvin <canaelgvin@gmail.com>
2021-09-27 12:17:03 +0200

    modified problem5_cana.cpp to include all functions, modified hpp files

---
+++ max_offdiag.cpp
@@ -0,0 +1,32 @@
+#include "max_offdiag.hpp"
+#include <armadillo>
+#include <assert.h>
+
+using namespace arma;
+
+double max_offdiag_symmetric(mat A, int &k, int &l)
+{
+
+    assert(A.is_square());
+
+    int n = sqrt(A.size());
+    cout << "n = " << n << endl;
+
+    //assigning minimum value to variable
+    double maxval = -1;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = i + 1; j < n; ++j)
+        {
+            double aij = fabs(A(i, j));
+            if (aij > maxval)
+            {
+                maxval = aij;
+                k = i;
+                l = j;
+            }
+        }
+    }
+
+    return maxval;
+}
